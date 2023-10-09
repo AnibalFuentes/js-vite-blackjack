285852c5cd939432ec7ed1b654144f05fad4524d
Anibal Fuentes <anibaldfuentesprieto@gmail.com>
2023-10-09 15:11:01 -0500

    First commit

---
+++ src/blackjack/usescases/index.js
@@ -0,0 +1,5 @@
+export { pedirCarta } from './pedir-carta';
+export {crearDeck} from './crear-deck';
+export {crearCartaHTML} from './crear-carta-html';
+export {turnoComputadora} from './turno-computadora';
+export {valorCarta} from './valor-carta';
